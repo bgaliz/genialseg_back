2b5e8b8bbd62630048299d02248fd3218413fbcd
Bruno Galiz <brunogaliz@hotmail.com>
2025-05-07 17:08:54 -0400

    fixing middleware!

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction): void => {
     const token = req.headers.authorization?.split(' ')[1];
-  
+
     if (!token) {
         res.status(401).json({ message: "Token não fornecido" });
         return;
@@ -11,15 +11,19 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction):
   
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+
         if (!decoded) {
             res.status(401).json({ message: "Token inválido" });
             return;
         }
         
-        req.body.role = decoded; // Adiciona o usuário decodificado ao objeto da requisição
-        next(); // Continua para a próxima função
+        if(req.body) {
+            req.body.role = decoded;
+        }
+        
+        next();
     } catch (error) {
         console.error("Erro ao verificar o token:", (error as Error).message);
-        res.status(401).json({ message: "Token inválido" });
+        res.status(401).json({ message: "Token inválido", error: (error as Error).message });
     }
 };
